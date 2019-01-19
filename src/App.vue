bd73955a5eccd2a9d6f9c9af5f1345a87d0c3c53
ごま <gomayumax@users.noreply.github.com>
2019-01-19 14:34:38 +0900

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <v-app>
+      <v-content>
+        <router-view/>
+      </v-content>
+    </v-app>
   </div>
 </template>
 
