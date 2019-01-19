04f5978845e2a4f38ba4061d1dc72be2c532fa51
ごま <gomayumax@users.noreply.github.com>
2019-01-19 16:40:15 +0900

    delete test routing

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Top from '@/components/Top'
 import Career from '@/components/Career'
-import Page1 from '@/components/Page1'
-import Page2 from '@/components/Page2'
 require('vue2-animate/dist/vue2-animate.min.css')
 
 Vue.use(Router)
@@ -19,16 +17,6 @@ export default new Router({
       path: '/career',
       name: 'Career',
       component: Career
-    },
-    {
-      path: '/page1',
-      name: 'Page1',
-      component: Page1
-    },
-    {
-      path: '/page2',
-      name: 'Page2',
-      component: Page2
     }
   ]
 })
