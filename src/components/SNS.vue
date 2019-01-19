226634d6e4ae15edb878b5a796a75938a5483234
ごま <gomayumax@users.noreply.github.com>
2019-01-19 14:35:05 +0900

    add page component

---
+++ src/components/SNS.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'SNS'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Top.vue
@@ -0,0 +1,14 @@
+<template>
+
+
+</template>
+
+<script>
+export default {
+  name: 'Top'
+}
+</script>
+
+<style scoped>
+
+</style>
